785ed80f63362ee7d52052d8bd69d3d6c6e29823
mjguggen <mjguggen@gmail.com>
2020-04-14 15:06:21 -0500

    Updates

---
+++ precache-manifest.de2cc96c79b6066ac0809b8c473402fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38c87fe972d70f128447a2d3213658e",
+    "url": "/NeonTrivia/index.html"
+  },
+  {
+    "revision": "14a04de47eaaf5d666ed",
+    "url": "/NeonTrivia/static/css/main.44a199dd.chunk.css"
+  },
+  {
+    "revision": "dd42ff7b50fb01692428",
+    "url": "/NeonTrivia/static/js/2.46c90771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NeonTrivia/static/js/2.46c90771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a04de47eaaf5d666ed",
+    "url": "/NeonTrivia/static/js/main.3364fb16.chunk.js"
+  },
+  {
+    "revision": "96a47e28e3bcc5fcd65c",
+    "url": "/NeonTrivia/static/js/runtime-main.53c3f5e3.js"
+  }
+]);
